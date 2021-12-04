8d3b3c5416936bd80717611f63632e402c6410dc
ola31 <abrahamola80@gmail.com>
2021-12-04 23:22:03 +0900

    modify max linvel, max angvel(2021-12-04)(laptop)

--- src/rosjoy_2_cmdvel.cpp
+++ src/rosjoy_2_cmdvel.cpp
@@ -30,8 +30,8 @@
 #define Back_Hand_Control_ 6
 
 /******* 조이스틱 속도 스케일링**********/
-#define MAX_LINEAR_VEL 1.0//1.25   //dymanic cmd_vel 모드에서 linear x 속도의 P gain에 해당
-#define MAX_ANGULAR_VEL 2.0 //2  //dynamic cmd_vel 모드에서 angular z 속도의 P gain에 해당
+#define MAX_LINEAR_VEL 0.75//1.25   //dymanic cmd_vel 모드에서 linear x 속도의 P gain에 해당
+#define MAX_ANGULAR_VEL 1.5 //2  //dynamic cmd_vel 모드에서 angular z 속도의 P gain에 해당
 
 
 /********조이스틱 Axes & Butten*******/
